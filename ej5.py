d61e5f72de23a85a386dab2d3ce40dde7ac9c4f2
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-03-20 11:17:16 -0300

    Se agregó el ejercicio 5

---
+++ ej5.py
@@ -0,0 +1,9 @@
+my_list = []
+num = int(input('Ingrese un numero '))
+while (num != 0):
+  my_list.append(num)
+  num = int(input('Ingrese un numero '))
+for i in range(len(my_list)):
+  if (my_list[i] < 0):
+    break
+  print(my_list[i])
