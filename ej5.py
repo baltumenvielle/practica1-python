3a0945ca2dd64f83e1f19597867b278829237888
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-03-20 12:14:11 -0300

    Se realizaron cambios

--- ej5.py
+++ ej5.py
@@ -1,8 +1,10 @@
 my_list = []
+
 num = int(input('Ingrese un numero '))
 while (num != 0):
   my_list.append(num)
   num = int(input('Ingrese un numero '))
+  
 for i in range(len(my_list)):
   if (my_list[i] < 0):
     break
