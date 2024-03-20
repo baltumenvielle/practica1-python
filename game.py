1154689f3bb9045b0014c5c13c97c1d17552870a
baltazarmenvielle <baltazarmenvielle@gmail.com>
2024-03-20 13:34:01 -0300

    Se arregló el bug y ahora se trabaja con fallos en vez de intentos

--- game.py
+++ game.py
@@ -3,24 +3,48 @@ import random
 # Lista de palabras posibles
 words = ["python", "programación", "computadora", "código", "desarrollo",
 "inteligencia"]
+vocals = ["a", "e", "i", "o", "u"]
 
 # Elegir una palabra al azar
 secret_word = random.choice(words)
-# Número máximo de intentos permitidos
-max_attempts = 10
+# Número máximo de fallos permitidos
+max_fails = 10
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
 print("¡Bienvenido al juego de adivinanzas!")
 print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 
+difficulty = int(input("Ingrese la dificultad en la que quieres jugar! 1. Fácil 2. Medio 3. Difícil "))
+
+# Valida que se ingrese una dificultad válida
+while (difficulty > 3) or (difficulty < 1):
+	difficulty = int(input("Ingresaste una dificultad no válida. Ingrese alguna de las siguientes: 1. Fácil 2. Medio 3. Difícil "))
+ 
 word_displayed = "_" * len(secret_word)
+
+if (difficulty == 1):
+	NotImplemented
+
+elif (difficulty == 2):
+	NotImplemented
+
+
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+# Inicializa la cantidad de fallos en 0
+fails = 0
+
+# Itera hasta que la cantidad de fallos alcance el maximo posible
+while (fails < max_fails):
 	# Pedir al jugador que ingrese una letra
 	letter = input("Ingresa una letra: ").lower()
+ 
+	# Si no se ingresó nada, se vuelve a intentar ingresar una letra
+	if letter == "":
+		print("No ingresaste ninguna letra. Intenta con otra")
+		continue
  
 	# Verificar si la letra ya ha sido adivinada
 	if letter in guessed_letters:
@@ -35,6 +59,7 @@ for i in range(max_attempts):
 		print("¡Bien hecho! La letra está en la palabra.")
 	else:
 		print("Lo siento, la letra no está en la palabra.")
+		fails += 1
  
 	# Mostrar la palabra parcialmente adivinada
 	letters = []
@@ -43,12 +68,14 @@ for i in range(max_attempts):
 			letters.append(letter)
 		else:
 			letters.append("_")
+   
 	word_displayed = "".join(letters)
 	print(f"Palabra: {word_displayed}")
+ 
 	# Verificar si se ha adivinado la palabra completa
 	if word_displayed == secret_word:
 		print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
 		break
-	else:
-		print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
-		print(f"La palabra secreta era: {secret_word}")
+else:
+	print(f"¡Oh no! Has agotado tus {max_fails} intentos.")
+	print(f"La palabra secreta era: {secret_word}")
